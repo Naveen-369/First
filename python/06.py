7410d77e86dd7ef421a06f14c33cc2f60845fed1
Naveen-369 <nk9363690@gmail.com>
2022-12-18 15:40:43 +0530

    PAN card Verifier

--- python/06.py
+++ python/06.py
@@ -1,25 +1,12 @@
-# Function to check whether the data entered by the employee is the valid PAN card number or not
-def pan_check(pan):
-    pan_list = list(pan)
-    # Checks the length of the PAN
-    if len(pan_list) != 10:
-        return False
-    # Checks the first half of the PAN
-    for i in range(0, 5):
-        if not pan_list[i].isalpha():
-            return False
-    # Checks the last character
-    if not pan_list[-1].isalpha():
-        return False
-    # Checks the remaining characters
-    for i in range(5, 9):
-        if not pan_list[i].isnumeric():
-            return False
-    return True
-
-# Main
-employee_pan = input("Enter the PAN number : ")
-if pan_check(employee_pan):
-    print("Valid")
-else:
+pan=input("Enter a valid PAN number : ")
+if len(pan)!=10:
     print("Invalid")
+a=pan[0:4]
+b=pan[-1]
+if a.isupper()and b.isupper():
+    e=1
+else:
+    e=0
+    print("Invalid")
+if e==1 and pan[5:9].isnumeric():
+    print("Valid")
