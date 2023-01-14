5878988b45cf5149580dca64a4b3cc1430caf593
Naveen-369 <nk9363690@gmail.com>
2023-01-14 09:43:30 +0530

    Refreshed

--- python/09.py
+++
@@ -1,7 +0,0 @@
-word=input("Enter a string : ")
-vowels=["a","i","e","o","u","A","I","E","O","U"]
-str=""
-for i in vowels:
-    f=word.replace(i, "")
-    word=f
-print(f)
--- python/14.py
+++
@@ -1,6 +0,0 @@
-import All
-All.buy()
-with open("details.txt", "r") as f:
-    g=f.readlines()
-    for i in range(len(g)):
-        print(g[i])
