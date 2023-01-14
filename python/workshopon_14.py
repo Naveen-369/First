337f06128dc8daf0111bc5c38620a83f8d2cbb02
Naveen-369 <nk9363690@gmail.com>
2023-01-14 11:15:34 +0530

    Creating Classes

--- python/workshopon_14.py
+++ python/workshopon_14.py
@@ -1,4 +1,22 @@
-import time
-for i in range(10):
-    print(i)
-    time.sleep(3)
+class friend():
+    def __init__(self,name, phone, email,instaId):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.instaId = instaId
+    def details(self):
+        print(self.name, self.phone, self.email, self.instaId)
+
+    def update(self, newName='', newPhone='0000000000', newEmail='', newInsta=''):
+        if newName!='':
+            self.name=newName
+        if newPhone!='0000000000':
+            self.phone=newPhone
+        if newEmail!='':
+            self.email = newEmail
+        if newInsta!='':
+            self.instaId = newInsta
+        self.details()
+friend1=friend(name='Kishan', phone='1329820498', email='kishansadeesh13@gmail.com' , instaId='lkasdjf' )
+friend1.details()
+friend1.update(newName='naveen')
