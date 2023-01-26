3ebf051ac1e7b83aae0c207e00ea9bf495c7554a
Naveen-369 <nk9363690@gmail.com>
2023-01-27 00:25:23 +0530

    Extra-curricular activities

--- python/Whatsapp_hack.py
+++ python/Whatsapp_hack.py
@@ -3,8 +3,8 @@ import pyautogui
 import time
 time.sleep(3)
 count=0
-while count<=333:#number of times
-    pyautogui.typewrite("Hello machn nalaiku FFCS Get up soon"+str(count))#word
+while count<=18:#number of times
+    pyautogui.typewrite("Hello Madam"+str(count))#word
     pyautogui.press("enter")
     count=count+1
     
