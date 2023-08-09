6657bffa892b65ff489c1b24f01254d030b20f49
Naveen <112697602+Naveen-369@users.noreply.github.com>
2023-08-09 18:20:35 +0530

    Update Wrapper.java

--- JAVACC/Wrapper.java
+++ JAVACC/Wrapper.java
@@ -1,6 +1,6 @@
 public class Wrapper {
     public static void main(String args[]){
-        Integer i=Integer.valueOf(789); //This is called "Boxing" and each of the Basic types are wrapped inside a classs called "wrapper class"
+        Integer i=Integer.valueOf(7999); //This is called "Boxing" and each of the Basic types are wrapped inside a classs called "wrapper class"
         System.out.println(i.intValue());   //.intValue() is for "Unboxing".
         Integer j=78;   //This is called "Auto-Boxing".
         System.out.println(j);  //Here we dont't use intValue() This is auto "unboxing".
